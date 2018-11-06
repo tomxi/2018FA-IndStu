8d25920c083bfbf571cd3d25deff96150ad1c922
tomxi <tomxi007@gmail.com>
2018-11-06 10:15:09 -0500

    created validation set, and tried training on different-sized training set

--- tom.py
+++ tom.py
@@ -26,7 +26,7 @@ class MelGenerator(keras.utils.Sequence):
     def _data_from_ids(self, n_ids):
         x_ids = self.sample_key[n_ids]
         x_batch = []
-        y_batch = np.multiply(self.Y_true[n_ids], self.Y_mask[n_ids])
+        y_batch = np.multiply(self.Y_true[n_ids], self.Y_mask[n_ids]) > 0.5
         for x_id in x_ids:
             mel_subdir = os.path.join(self.mel_basedir, x_id[:3])
             melpath = os.path.join(mel_subdir, x_id+'.npz')
@@ -35,7 +35,10 @@ class MelGenerator(keras.utils.Sequence):
         x_batch = np.array(x_batch)
         x_batch = np.expand_dims(x_batch, axis=-1)
         
-        return x_batch, y_batch
+        return x_batch.astype('float32'), y_batch
+    
+    def on_epoch_end(self):
+        np.random.shuffle(self.n_ids)
 
 class MelParam(NamedTuple):
     sr: int
@@ -74,7 +77,7 @@ def save_melspec(x_id,
             print("bad shape! ID:{}, shape: {}".format(x_id, melspec.shape))        
         np.savez(mel_path, x_id=x_id, melspec=melspec)
 
-def construct_crnnL3_smp_tom(input_shape=(None, 128, 1), alpha=0.5, n_classes=20):
+def construct_crnnL3_smp_tom(input_shape=(None, 128, 1), n_classes=20):
     '''
     Modified from Openmic's original models.py file
     CRNN with L3-style conv encoder
@@ -155,16 +158,15 @@ def construct_crnnL3_smp_tom(input_shape=(None, 128, 1), alpha=0.5, n_classes=20
 
     # RNN
     # First recurrent layer: a 128-dim bidirectional gru
-    rnn1 = keras.layers.Bidirectional(keras.layers.GRU(128,
-                                                       return_sequences=True))(sq2)
+#     rnn1 = keras.layers.Bidirectional(keras.layers.GRU(128,
+#                                                        return_sequences=True))(sq2)
 
     p0 = keras.layers.Dense(n_classes, activation='sigmoid')
 
-    p_dynamic = keras.layers.TimeDistributed(p0, name='dynamic/tags')(rnn1)
+    p_dynamic = keras.layers.TimeDistributed(p0, name='dynamic/tags')(sq2) #rnn1
 
-    p_static = layers.SoftMaxPool(alpha=alpha,
-                                  axis=1,
-                                  name='static/tags')(p_dynamic)
+    p_static = layers.AutoPool(axis=1,
+                               name='static/tags')(p_dynamic)
 
     model = keras.models.Model(input_feature,
                                p_static)
